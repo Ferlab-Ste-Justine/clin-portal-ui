de620dd8ecfd4dc370724e17cb87772f6ccac6e8
Emmanuel Nau <enau@ferlab.bio>
2024-06-27 10:30:46 -0400

    feat: CLIN-2959 Update to ferlabui

--- src/utils/translation.ts
+++ src/utils/translation.ts
@@ -55,7 +55,6 @@ export const getProTableDictionary = (): IProTableDictionary => ({
   itemCount: {
     result: intl.get('protable.results'),
     results: intl.get('protable.results'),
-    result: intl.get('protable.result'),
     noResults: intl.get('protable.noResults'),
     of: intl.get('protable.of'),
     selected: intl.get('protable.selected'),
